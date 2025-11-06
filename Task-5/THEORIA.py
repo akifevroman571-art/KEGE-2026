c0081e468ec2b15f6504810d7373749f76388962
Roman23 <royache@mail.ru>
2025-11-06 17:45:53 +0200

    TASK-5

---
+++ Task-5/THEORIA.py
@@ -0,0 +1,44 @@
+# Система счисления
+# двоичная система
+N = 25
+print(bin(N)[2:])
+print(f'{N:b}')
+
+# Восьмеричная система
+print(oct(N)[2:])
+print(f'{N:0}')
+
+
+# Шестнадцатеричная система
+print(hex(N)[2:])
+print(f'{N:x}')
+
+# Перевод в любую систему(2 <= sys <=9)
+def convert(num, sys):
+    res = ''
+    while num:
+        res += str( num % sys)
+        num //= 3
+    return res[::-1]
+
+
+# Перевод в любую систему(2 <= sys <=36)
+
+from string import printable as pp
+def convert(num, sys):
+    res = ''
+    alph = pp
+    while num:
+        res += alph[num % sys]
+        num //= 3
+    return res[::-1]
+
+# Перевод в 10-ую систему счисления
+bin_num = '101'
+oct_num = '765'
+tri_num = '1201'
+print(int(bin_num), 2)
+print(int(oct_num), 8)
+print(int(tri_num), 3)
+
+
