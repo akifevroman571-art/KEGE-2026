30d479d1aec134c83b670d7ed18551ceff6a9b29
Roman23 <royache@mail.ru>
2025-12-25 17:25:34 +0200

    25.12.2025

--- task-2/theoria.py
+++ task-2/theoria.py
@@ -25,3 +25,34 @@ for a in range(2):
                 if not f:
                     print(a, b, c, d)
                 print(a, b, c, d)
+
+# args
+def f1(a, b, c):
+    return a+b+c
+test = [1, 2, 3]
+print(f1(*(test)))
+
+# kwargs
+def f2(a, b):
+    return a / b
+
+
+test2 = {'a':5, 'b':2}
+print(f2(**test2))
+
+# убийца второго задания
+from itertools import product, permutations
+
+def f(x, y, z, w):
+    return (x or y) and not (y == z) and not w
+
+for i in product((0, 1), repeat=4):
+    table = [
+        (1, i[0], 1, i[1]),
+        (0, 1, i[2], 0),
+        (i[3], 1, 1, 0)
+    ]
+    if len(set(table)) == len(table):
+        for p in permutations('xyzw'):
+            if [f(**dict(zip(p, t))) for t in table] == [1, 1, 1]:
+                print(*p)
