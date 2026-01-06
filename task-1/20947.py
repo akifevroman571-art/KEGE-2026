62ad3412acbda0aa3bafeca8d5d5302b5c84924f
dz <royache@mail.ru>
2026-01-06 20:40:57 +0200

    06 01 26

--- task-1/20947.py
+++ task-1/20947.py
@@ -3,6 +3,6 @@ from itertools import *
 matrix = '267 157 468 356 248 134 12 35'.split()
 graph = 'AB AC CB BG BD GJ DJ DU DE GU EU'.split()
 print(*range(1, 9))
-for i in permutations('ABCDEFGH'):
+for i in permutations('ABCDEJUG'):
     if all(str(i.index(x)+1) in matrix[i.index(y)] for x, y in graph):
         print(*i)
