c979670a02c7fc623430f6fd4f627a213ce50623
Roman23 <royache@mail.ru>
2026-02-19 17:52:25 +0200

    bebebe boris olegovich

--- task-8/10921.py
+++ task-8/10921.py
@@ -1,12 +1,6 @@
 from itertools import *
 a = 'ДЖАВАСКРИПТ'
-for val in permutations(a):
+ans = 0
+for val in set(permutations(a)):
     val=''.join(val)
-    for i in val:
-        if i in 'АИ':
-            b = val.index('А')
-            c = val.index('И')
-            ans = b + c + 1
-print(ans)
-
-
+    if
