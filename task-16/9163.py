57769f9f7b94fdb44185cba701ec6a4195a82ecb
Roman23 <royache@mail.ru>
2026-02-19 17:52:39 +0200

    bebebe boris olegovich

---
+++ task-16/9163.py
@@ -0,0 +1,8 @@
+from functools import lru_cache
+@lru_cache(None)
+def f(n):
+    if n >= 2025: return n
+    return f(n+1) - f(n+2)+7
+for i in range(2050, 0, -1):
+    f(i)
+print(f(15) - f(24))
---
+++ task-23/5236.py
@@ -0,0 +1,3 @@
+def f(start, end, cnt):
+    if start == end: return 1
+    if start > end: return 0
