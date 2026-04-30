5e24e53cae4e752247d27f9791a13c64782dff79
Roman23 <royache@mail.ru>
2026-04-30 17:52:29 +0200

    24

---
+++ task-24/type-1/1975-perebor.py
@@ -0,0 +1,12 @@
+with open(r'../files-24/24_1975.txt') as file:
+    data = file.readline()
+cnt = 1
+ans = 0
+for i in range(len(data)-1):
+    if data[i] + data[i+1] != 'PP':
+        cnt+=1
+    else:
+        ans = max(ans, cnt)
+        cnt = 1
+ans = max(ans, cnt)
+print(ans)
--- task-24/type-1/1975.py
+++ task-24/type-1/1975.py
@@ -1,4 +1,4 @@
-with open(r'.\files-24\24_1975.txt') as file:
+with open(r'../files-24/24_1975.txt') as file:
     data = file.readline()
 
 
