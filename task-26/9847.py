85d1b9369757869ef93ea24b13da810895932715
Roman23 <royache@mail.ru>
2026-06-12 15:13:04 +0200

    vsdvsvsv

--- task-26/9847.py
+++ task-26/9847.py
@@ -6,13 +6,8 @@ cnt=0
 times = sorted(times)
 time_line = [0] * 60 * 24
 
-for time in times:
-    for i in range(time[0], time[1]):
-        time_line[i]+=1
-print(time_line)
-print(max(time_line))
 
-for i in range(len(time_line)-1):
-    if time_line[i]==time_line[i+1]==max(time_line):
-        cnt+=1
-print(cnt)
+
+print(time_line)
+
+print(len(times))
